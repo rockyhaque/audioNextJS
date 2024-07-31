dee3622de60e995ffd36fc64bc990573ea67a9cd
Rocky Haque <rockyhaque99@gmail.com>
2024-07-31 16:46:12 +0600

    whychooseus section done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import FeaturesCourses from "@/components/FeaturesCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection></HeroSection>
+      <FeaturesCourses></FeaturesCourses>
+      <WhyChooseUs></WhyChooseUs>
     </main>
   );
 }
